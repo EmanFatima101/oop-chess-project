e6b5b18c91c4b89048bcf6044f9d490ccb1276cb
EmanFatima101 <emanfatima151195@gmail.com>
2026-04-25 09:31:52 -0700

    Implemented Piece class structure
    Includes Pawn, Rook, Knight, Bishop, Queen, King classes with inheritance and basic movement logic.

---
+++ piece.cpp
@@ -0,0 +1,120 @@
+#include"piece.h"// Including header file
+
+Piece::Piece(string c, int r, int co, char sym)
+	:color(c), row(r), col(co), symbol(sym)
+{
+
+}
+string Piece::getColor()
+{
+	return color;
+}
+char Piece::getSymbol()
+{
+	return symbol;
+}
+int Piece::getRow()
+{
+	return row;
+}
+
+int Piece::getCol()
+{
+	return col;
+}
+void Piece::setPosition(int r, int co)
+{
+	//here updating he positions 
+	row = r;
+	col = co;
+}
+
+//***********************************
+//===========PAWN implementation=====
+//***********************************
+Pawn::Pawn(string c, int r, int co, char sym)
+	: Piece(c, r, co, 'P')
+{
+}
+bool Pawn::isValid_Move(int r, int co)
+{
+	// White moves upward
+	if (getColor() == "white")
+	{
+		return (r == getRow() - 1 && co == getCol());
+
+	}
+	// Black moves downward
+	else
+	{
+		return (r == getRow() + 1 && co == getCol());
+	}
+}
+
+//***********************************
+//===========ROCK implementation=====
+//***********************************
+Rook::Rook(string c, int r, int co, char sym)
+	: Piece(c, r, co, 'R')
+{
+}
+
+bool Rook::isValid_Move(int r, int co)
+{
+	return (r == getRow() || co == getCol());
+}
+
+//***********************************
+//===========Knight implementation=====
+//***********************************
+Knight::Knight(string c, int r, int co, char sym)
+	: Piece(c, r, co, 'K')
+{
+}
+
+bool Knight::isValid_Move(int r, int co)
+{
+
+	int rowDiff = abs(r - getRow());
+	int colDiff = abs(co - getCol());
+	return (rowDiff == 2 && colDiff == 1) || (rowDiff == 1 && colDiff == 2);
+
+}
+
+
+//***********************************
+//===========BISHOP implementation=====
+//***********************************
+Bishop::Bishop(string c, int r, int co, char sym)
+	: Piece(c, r, co, 'B')
+{
+}
+bool Bishop::isValid_Move(int r, int co)
+{
+	return abs(r - getRow()) == abs(co - getCol());
+}
+//***********************************
+//===========QUEEN implementation=====
+//***********************************
+Queen::Queen(string c, int r, int co, char sym)
+	: Piece(c, r, co, 'Q')
+{
+}
+bool Queen::isValid_Move(int r, int co)
+{
+	return (r == getRow() || co == getCol()) || (abs(r - getRow()) == abs(co - getCol()));
+}
+//***********************************
+//===========KING implementation=====
+//***********************************
+
+King::King(string c, int r, int co, char sym)
+	: Piece(c, r, co, 'K')
+{
+}
+bool King::isValid_Move(int r, int co)
+{
+	int rowDiff = abs(r - getRow());
+	int colDiff = abs(co - getCol());
+	return (rowDiff <= 1 && colDiff <= 1);
+}
---
+++ piece.h
@@ -0,0 +1,81 @@
+//File name is piece.h to follow naming convension
+#pragma once
+#include<iostream>
+#include<string>
+#include<cmath>   
+using namespace std;
+
+class Piece
+{
+	string color;//stores white or black
+	int row;//current row position
+	int col;//current column position
+	char symbol;// p(pawn),R(Rook),K( Knight), B(Bishop), Q(Queen),Ki( King))
+
+public:
+	Piece(string c, int r, int co, char sym);
+	/*:color(c),row(r),col(co),symbol(sym)*/
+
+	string getColor();
+	char getSymbol();
+	int getRow();
+	int getCol();
+	//because we cannot access them directly
+	void setPosition(int r, int co);//informing about the positions updating 
+
+	virtual bool isValid_Move(int r, int co) = 0;//Pure virtual function bcz there is no default behaviour of this function in the base class
+	//Every piece must define its own movement
+};
+class Pawn :public Piece
+{
+public:
+	Pawn(string c, int r, int co, char sym);
+
+	bool isValid_Move(int r, int co) override;
+	//Every piece must define its own movement rule
+	// a pawn can only move forward one square (or two squares on its first move)
+};
+class Rook : public Piece
+{
+public:
+	Rook(string c, int r, int co, char sym);
+
+	bool isValid_Move(int r, int co) override;
+	//Every piece must define its own movement
+	//a rook can move in + shape but cannot jump over other pieces
+};
+class Knight :public Piece {
+public:
+	Knight(string c, int r, int co, char sym);
+
+	bool isValid_Move(int r, int co) override;
+	//Every piece must define its own movement
+	//Knight moves in an L shape and can jump over other pieces
+};
+
+class Bishop :public Piece {
+public:
+	Bishop(string c, int r, int co, char sym);
+
+	bool isValid_Move(int r, int co) override;
+	//Every piece must define its own movement
+	//Bishop moves diagonally and cannot jump over other pieces
+
+};
+class Queen : public Piece {
+public:
+	Queen(string c, int r, int co, char sym);
+
+	bool isValid_Move(int r, int co) override;
+	//Queen piece  must define its own movement
+	//Queen can move in any direction (horizontally, vertically, diagonally) but cannot jump over other pieces
+	//Queen movement combines both the rook and bishop.
+
+};
+class King : public Piece {
+public:
+	King(string c, int r, int co, char sym);
+	bool isValid_Move(int r, int co) override;
+	//King can move one square in any direction (horizontally, vertically, diagonally) but cannot jump over other pieces
+	//The king is the most important piece in chess. The objective of the game is to checkmate the opponent's king.
+};
